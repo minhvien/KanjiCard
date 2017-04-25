e084b930a4333d2269a39b045deef83de8a52eb9
Tran Minh Vien <tmvien@tma.com.vn>
2017-04-25 16:56:29 +0700

    Init source.

---
+++ app/src/main/java/com/noname/tmvien/kanjicards/models/Levels.java
@@ -0,0 +1,12 @@
+package com.noname.tmvien.kanjicards.models;
+
+import com.google.firebase.database.IgnoreExtraProperties;
+
+/**
+ * Created by tmvien on 4/25/17.
+ */
+@IgnoreExtraProperties
+public class Levels {
+
+}
+
---
+++ app/src/main/java/com/noname/tmvien/kanjicards/ui/ApplicationConfigureActivity.java
@@ -0,0 +1,12 @@
+package com.noname.tmvien.kanjicards.ui;
+
+import android.preference.PreferenceActivity;
+
+/**
+ * Created by tmvien on 4/25/17.
+ */
+public class ApplicationConfigureActivity extends PreferenceActivity {
+
+    private static final String TAG = ApplicationConfigureActivity.class.getSimpleName();
+    public static final boolean DEBUG_MODE = true;
+}
